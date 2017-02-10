d083b5cdc48797c147ae5a9081e8ebd5e3acf33d
RickR <rrein@akamai.com>
2017-02-09 18:12:16 -0700

    Adds staff notification of help menu button presses. Fixes #19

--- lib/button_press/help_menu.rb
+++ lib/button_press/help_menu.rb
@@ -2,6 +2,10 @@ class ButtonPress::HelpMenu < ButtonPress
   def process
     help_klass = "HelpMenu::#{@action['name'].classify}".constantize
     Operationcode::Slack::Im.new(user: @user['id'], text: help_klass.text).deliver
+    Operationcode::Slack::Im.new(
+      channel: Event::STAFF_NOTIFICATION_CHANNEL,
+      text: "User #{@user['name']} has just clicked the #{@action['name']} button"
+    ).deliver
     nil
   end
 
