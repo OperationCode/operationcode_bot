04a2dbf9d4c493e0da9f7e232850e50b01429353
Harry Levine <hmlevine@gmail.com>
2017-10-09 19:53:47 -0600

    Uses module namespacing syntax
    Nesting a class in a class is uncommon for this purpose.
    
    Traditionally, when a class is in a folder for namespacing, you either nest the class in a module, or use this namespacing :: syntax.
    
    Here we are using the :: syntax.

--- lib/event/message.rb
+++ lib/event/message.rb
@@ -4,69 +4,67 @@ require 'operationcode/slack'
 # * Be assigned to the least populated mentorship squad
 # * Have their squad and user info added to the Mentorship air table
 # * Be invited to the squad's slack private channel
-class Event
-  class Message < Event
-    attr_reader :user
+class Event::Message < Event
+  attr_reader :user
 
-    ACTIONABLE_KEYWORD = 'yes'
+  ACTIONABLE_KEYWORD = 'yes'
 
-    def initialize(data, token: nil, logger: nil)
-      @message = data['event']['text']
-      @user = Operationcode::Slack::User.new(data['event']['user'])
-      @channel = data['event']['channel']
+  def initialize(data, token: nil, logger: nil)
+    @message = data['event']['text']
+    @user = Operationcode::Slack::User.new(data['event']['user'])
+    @channel = data['event']['channel']
 
-      super
+    super
+  end
+
+  def process
+    im = Operationcode::Slack::Im.new(user: user.id, channel: @channel, text: "I'm sorry. I don't know how to talk to humans yet. Here's what I do know.")
+    im.make_interactive_with!(HelpMenu.generate_interactive_message)
+    im.deliver
+  end
+
+  private
+
+  # The rest of this has to do with actionable items
+  # and will be moved over soon
+  def user_wants_to_join?
+    @data['event']['text'].downcase != 'no'
+  end
+
+  def add_user
+    if user_exists?
+      log "user #{user.name} already exists"
+    else
+      invite_user_to least_populated_squad
+      save_user_to_airtables!
     end
+  end
 
-    def process
-      im = Operationcode::Slack::Im.new(user: user.id, channel: @channel, text: "I'm sorry. I don't know how to talk to humans yet. Here's what I do know.")
-      im.make_interactive_with!(HelpMenu.generate_interactive_message)
-      im.deliver
+  def user_exists?
+    Airtables::MentorshipSquads.user_exists?(user.name)
+  end
+
+  def invite_user_to(squad)
+    channel_id = Squad.channel_id_for squad
+
+    log "Inviting #{user.id} (#{user.name}) to channel #{channel_id} (#{squad})"
+    if ENV['INVITE_USER'] == 'true'
+      Operationcode::Slack::Api::ChannelsInvite.post(
+        with_data: {
+          token: @token,
+          channel: channel_id,
+          user: user.id
+        }
+      )
     end
+  end
 
-    private
+  def save_user_to_airtables!
+    log "Adding slack username #{user.name} to squad #{@squad} to airtables"
+    Airtables::MentorshipSquads.create({slack_username: user.name, squad: @squad})
+  end
 
-    # The rest of this has to do with actionable items
-    # and will be moved over soon
-    def user_wants_to_join?
-      @data['event']['text'].downcase != 'no'
-    end
-
-    def add_user
-      if user_exists?
-        log "user #{user.name} already exists"
-      else
-        invite_user_to least_populated_squad
-        save_user_to_airtables!
-      end
-    end
-
-    def user_exists?
-      Airtables::MentorshipSquads.user_exists?(user.name)
-    end
-
-    def invite_user_to(squad)
-      channel_id = Squad.channel_id_for squad
-
-      log "Inviting #{user.id} (#{user.name}) to channel #{channel_id} (#{squad})"
-      if ENV['INVITE_USER'] == 'true'
-        Operationcode::Slack::Api::ChannelsInvite.post(
-          with_data: {
-            token: @token,
-            channel: channel_id,
-            user: user.id
-          }
-        )
-      end
-    end
-
-    def save_user_to_airtables!
-      log "Adding slack username #{user.name} to squad #{@squad} to airtables"
-      Airtables::MentorshipSquads.create({slack_username: user.name, squad: @squad})
-    end
-
-    def least_populated_squad
-      @squad ||= Airtables::MentorshipSquads.least_populated
-    end
+  def least_populated_squad
+    @squad ||= Airtables::MentorshipSquads.least_populated
   end
 end
--- lib/event/team_join.rb
+++ lib/event/team_join.rb
@@ -6,51 +6,49 @@ require 'operationcode/slack'
 #   * Be assigned to the least populated mentorship squad
 #   * Have their squad and user info added to the Mentorship air table
 #   * Be invited to the squad's slack private channel
-class Event
-  class TeamJoin < Event
-    attr_reader :user
+class Event::TeamJoin < Event
+  attr_reader :user
 
-    def initialize(data, token: nil, logger: nil)
-      super
-      @template = File.read(template_path + 'welcome_message.txt.erb')
-    end
+  def initialize(data, token: nil, logger: nil)
+    super
+    @template = File.read(template_path + 'welcome_message.txt.erb')
+  end
 
-    def process
-      @user = Operationcode::Slack::User.new(@data['event']['user'])
-      log "Production mode: #{production_mode?}"
-      log "Welcoming user #{resolve_user_name}"
+  def process
+    @user = Operationcode::Slack::User.new(@data['event']['user'])
+    log "Production mode: #{production_mode?}"
+    log "Welcoming user #{resolve_user_name}"
 
-      welcome_user!
-      notify_community!
-    end
+    welcome_user!
+    notify_community!
+  end
 
-    private
+  private
 
-    def welcome_user!
-      im = Operationcode::Slack::Im.new(user: resolve_user_name, text: ERB.new(@template).result(binding))
-      im.make_interactive_with!(HelpMenu.generate_interactive_message)
-      im.deliver
-    end
+  def welcome_user!
+    im = Operationcode::Slack::Im.new(user: resolve_user_name, text: ERB.new(@template).result(binding))
+    im.make_interactive_with!(HelpMenu.generate_interactive_message)
+    im.deliver
+  end
 
-    def notify_community!
-      im = Operationcode::Slack::Im.new(
-        channel: Event::COMMUNITY_CHANNEL,
-        text: ":tada: <@#{user.id}> has joined the Slack team :tada:"
-        )
-      im.make_interactive_with!(
-        Operationcode::Slack::Im::Interactive.new(
-          text: 'Have they been greeted via direct message?',
-          id: 'greeted',
-          actions: [
-            {name: 'yes', text: 'Yes', value: 'yes'}
-          ]
-        )
+  def notify_community!
+    im = Operationcode::Slack::Im.new(
+      channel: Event::COMMUNITY_CHANNEL,
+      text: ":tada: <@#{user.id}> has joined the Slack team :tada:"
       )
-      im.deliver
-    end
+    im.make_interactive_with!(
+      Operationcode::Slack::Im::Interactive.new(
+        text: 'Have they been greeted via direct message?',
+        id: 'greeted',
+        actions: [
+          {name: 'yes', text: 'Yes', value: 'yes'}
+        ]
+      )
+    )
+    im.deliver
+  end
 
-    def resolve_user_name
-      production_mode? ? user.id : 'U08U56D5K'
-    end
+  def resolve_user_name
+    production_mode? ? user.id : 'U08U56D5K'
   end
 end
