ee52ededdf8740353176ad23d81881d0549e7615
John Hampton <john_j_hampton@yahoo.com>
2017-10-15 19:07:47 -0400

    Fix code climate errors (indentation)

--- lib/common.rb
+++ lib/common.rb
@@ -1,5 +1,4 @@
 module Common
-
     # Make sure we are in dev mode unless explicitly disabled
     def production_mode?
         ENV['PRODUCTION_MODE'] == 'true' ? true : false
@@ -8,4 +7,4 @@ module Common
     def resolve_user_name
         production_mode? ? self.user.id : 'U08U56D5K'
     end
-  end
+end
